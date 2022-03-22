cf43615d0f2a026e293324e06bcd9a9ae7524819
Bradford <945242167@qq.com>
2022-03-22 22:24:54 +0800

    增加profile

--- crawler/knowledgecenter/parser/article.go
+++ crawler/knowledgecenter/parser/article.go
@@ -1,1 +1,29 @@
 package parser
+
+import (
+	"crawler/engine"
+	"crawler/model"
+	"regexp"
+)
+
+var updatedAtRe = regexp.MustCompile(`<i>上次更新日期：(.*)</i>`)
+var titleRe = regexp.MustCompile(`<h1.*>(.*)</h1>`)
+
+func parseArticle(contents []byte) engine.ParseResult {
+	profile := model.Profile{}
+	profile.UpdatedAt = extractString(contents, updatedAtRe)
+	profile.Title = extractString(contents, titleRe)
+	result := engine.ParseResult{
+		Items: []interface{}{profile},
+	}
+	return result
+}
+
+func extractString(contents []byte, re *regexp.Regexp) string {
+	matches := re.FindSubmatch(contents)
+	if len(matches) >= 2 {
+		return string(matches[1])
+	} else {
+		return ""
+	}
+}
---
+++ crawler/model/profile.go
@@ -0,0 +1,11 @@
+package model
+
+type Profile struct {
+	Title              string
+	UpdatedAt          string
+	Question           string
+	ShortDescription   string
+	Resolution         string
+	RelatedInformation string
+	RelatedVideos      string
+}
