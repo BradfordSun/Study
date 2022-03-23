73c1c69c3a793435886284f0981c4d7bbe94e660
Bradford <945242167@qq.com>
2022-03-23 16:19:34 +0800

    需改testgit

--- testgit/main.go
+++ testgit/main.go
@@ -2,7 +2,13 @@ package main
 
 import "fmt"
 
+func add(a, b *int) {
+	*a, *b = *b, *a
+}
+
 func main() {
-	fmt.Println("Just a test")
-	fmt.Println("another test")
+	a := 10
+	b := 20
+	add(&a, &b)
+	fmt.Println(a, b)
 }
